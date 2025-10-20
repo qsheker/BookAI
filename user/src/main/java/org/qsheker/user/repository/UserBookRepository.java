069dad29e7e3f973e9a50b049f9235ede1b762a3
Aldiyar <aldikzhaks@gmail.com>
2025-10-20 13:54:02 +0500

    add repository layer

--- user/src/main/java/org/qsheker/user/repository/UserBookRepository.java
+++ user/src/main/java/org/qsheker/user/repository/UserBookRepository.java
@@ -1,4 +1,14 @@
 package org.qsheker.user.repository;
 
-public interface UserBookRepository {
+import org.qsheker.user.models.entity.UserBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserBookRepository extends JpaRepository<UserBook, Long> {
+    Optional<UserBook> findByUserIdAndBookId(Long userId, Long bookId);
+
+    boolean existsUserBookByBookIdAndUserId(Long bookId, Long userId);
 }
--- user/src/main/java/org/qsheker/user/repository/UserRepository.java
+++ user/src/main/java/org/qsheker/user/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package org.qsheker.user.repository;
 
-public interface UserRepository {
+import org.qsheker.user.models.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
